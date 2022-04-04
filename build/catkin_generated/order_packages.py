62f7b93aa32d5be08e2c06dde2de96213e5ea086
TKEglin <eglin97@hotmail.com>
2022-04-04 07:46:10 +0000

    test

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/ubuntu/catkin_ws2/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/ubuntu/catkin_ws2/devel;/opt/ros/melodic'.split(';') if '/home/ubuntu/catkin_ws2/devel;/opt/ros/melodic' != '' else []
---
+++ build/turtlebot3_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/catkin_ws2/src/turtlebot3_msgs
+# Build directory: /home/ubuntu/catkin_ws2/build/turtlebot3_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
